902bacb5855d4b2e3b0728842468d9a1ef5c0b52
Prajwal-tagde <prajwaltagde8@gmail.com>
2023-11-14 16:37:17 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,47 @@
+const express = require('express');
+const cors = require('cors');
+require("dotenv").config();
+
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+
+app.get("/", (req, res) => {
+    res.send("hello");
+})
+
+app.post("/api/create-checkout-session", async (req, res) => {
+    const {products} = req.body;
+    // console.log(product);
+
+    const lineItems = products.map((product) => ({
+        price_data: {
+            currency: "inr",
+            product_data: {
+                name: product.dish
+            },
+            unit_amount: product.price * 100,
+        },
+        quantity: product.qnty
+    })
+    )
+
+    const session = await stripe.checkout.sessions.create({
+        payment_method_types: ["card"],
+        line_items: lineItems,
+        mode: "payment",
+        success_url: "http://localhost:5173/success",
+        cancel_url: "http://localhost:5173/cancel"
+    })
+
+    res.json({ id: session.id });
+
+})
+
+app.listen(7000, () => {
+    console.log("server started...");
+})
